489b0958b3a693a91e4d50d08f939ca3e8d17300
Markov Sergey <markov@qtech.ru>
2019-07-16 20:47:15 +0300

    закончил всю верстку

--- gulpfile.js
+++ gulpfile.js
@@ -86,7 +86,8 @@ gulp.task("copy", function () {
   return gulp.src([
     "source/fonts/**/*.{woff,woff2}",
     "source/img/**",
-    "source/css/**"
+    "source/css/**",
+    "source/download/**"
   ], {
     base: "source"
   })
